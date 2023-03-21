506ee7ca308ef81572e56b408c4ed91778dd45c1
EMANUEL2552 silva <emanuel.fernandesdasilva@gmail.com>
2023-03-21 19:47:28 +0000

    curso-bigdataBI

--- sql.sql
+++ sql.sql
@@ -13,6 +13,15 @@ insert into alunos values ( 4, "Maria", "Franca", 33);
 insert into alunos values ( 5, "Sonia", "Franca", 14);
 insert into alunos values ( 6, "rafael", "Franca", 20);
 
-
+CREATE Table 
+insert INTo alunos values655655
+sddd BIGINT INT 5665sdds -- Add a new column 'NewColumnName' to table 'TableName' in schema 'SchemaName'
+ALTER TABLE SchemaName.TableName
+    ADD NewColumnName /*new_column_name*/ int /*new_column_datatype*/ NULL /*new_column_nullability*/
+GO
+-- Get a list of tables and views in the current database
+SELECT table_catalog [database], table_schema [schema], table_name name, table_type type
+FROM INFORMATION_SCHEMA.TABLES
+GOassdas
 
 
